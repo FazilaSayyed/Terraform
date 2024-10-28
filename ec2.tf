d15b4b68d0445fdbaa35cded1d5ba4f68e38024d
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-10-28 12:00:22 +0530

    Update and rename Iamuser.tf to ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "this_ubuntu" {
+
+    ami = "ami-0325498274077fac5" 
+    disable_api_stop  = true 
+    disable_api_termination = true  
+    instance_type = "t2.micro" 
+    key_name = "navidali"
+    count = 4  #loop 
+    tags = {
+      purpose = "fazila"
+    } 
+    
+
+} 
