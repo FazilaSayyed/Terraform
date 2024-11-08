4befbc266a9ca8bf8686128f43865917b9940451
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-08 11:11:17 +0530

    commit

---
+++ RDS-mysql/variable.tf
@@ -0,0 +1,5 @@
+variable "this_list" {
+    type = list 
+    default = ["20" , "mysql" , "8.0" , "db.t4g.micro" , "mydatabase_mysql_1" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0f43f4aa1dbd5c852" , "false"]
+     
+}
