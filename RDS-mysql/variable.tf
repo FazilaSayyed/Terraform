3d33b6e0a89610561f979c78b481ffb5257768bb
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-08 11:33:45 +0530

    Update variable.tf

--- RDS-mysql/variable.tf
+++ RDS-mysql/variable.tf
@@ -1,5 +1,5 @@
 variable "this_list" {
     type = list 
-    default = ["20" , "mysql" , "8.0" , "db.t4g.micro" , "mydatabase_mysql_1" , "admin" , "admin123" , "default.mysql8.0" , "true" , "true" , "sg-0f43f4aa1dbd5c852" , "false"]
+    default = ["20" , "mysql" , "8.0" , "db.t4g.micro" , "mydatabase_mysql_1" , "admin" , "admin123" , "default.mysql8.0" , "true" , "true" , "sg-0063c7dcb89f09c9b" , "false"]
      
 }
