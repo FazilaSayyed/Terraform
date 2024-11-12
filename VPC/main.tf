00cca9e2c6297a8d8a65e704a3c438400e1557fe
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-12 11:27:46 +0530

    commit

--- VPC/main.tf
+++ VPC/main.tf
@@ -56,16 +56,4 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Output the VPC ID
-output "vpc_id" {
-  value = aws_vpc.main_vpc.id
-}
 
-# Output the Subnet IDs
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
-}
