9fb60c9a46cfcea063cdf952c036a528742631fc
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-12 11:25:18 +0530

    comm

--- VPC/main.tf
+++ VPC/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name = "PrivateSubnet"
