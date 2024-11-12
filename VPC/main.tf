6ca54cb071d850526965f5ce5e740831ec636255
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-12 11:24:07 +0530

    commit

--- VPC/main.tf
+++ VPC/main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-1"
+  availability_zone       = "us-west-1a"
 
   tags = {
     Name = "PublicSubnet"
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-1"
+  availability_zone = "us-west-1b"
 
   tags = {
     Name = "PrivateSubnet"
