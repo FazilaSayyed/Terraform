f37def839047c70357fa9e74ec11447ec02c9f1a
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-13 20:17:33 +0530

    commit

---
+++ static-web-hosting/variableblock.tf
@@ -0,0 +1,44 @@
+variable this_aws_region {
+    type = string 
+    default = "us-west-1"
+    description = "This variable is for aws region"
+}
+
+variable "this_ami" {
+    type = string
+    default = "ami-0da424eb883458071"
+    description = "This variable is for aws ami"
+}
+
+variable "this_instance_type" {
+    type = list
+    default = ["t2.micro" , "t3.small" , "t3.large"]
+    description = "This variable is for aws instance type"
+}
+
+variable "this_key_pair" {
+    type = string
+    default = "navidali"
+    description = "This variable is for aws key pair"
+}
+
+variable "this_associate_public_ip" {
+    type = bool
+    default = true 
+    description = "This variable is for aws assoociate public ip address"
+}
+
+variable "this_tag" {
+    type = map 
+    default = {
+        Name = "StaticWebHostingInstance"
+        Enivronment ="Dev"
+    }
+    description = "This variable is for aws instance tagging"
+}
+
+variable "this_security_group" {
+    type = string
+    default = "sg-0063c7dcb89f09c9b"
+    description = "This variable is for aws security group" 
+}
