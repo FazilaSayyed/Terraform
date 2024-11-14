8b242338c931f16cf3ddb5c1e06a28cd95aee4d3
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-14 23:06:17 +0530

    co

--- Data block/data.tf
+++ Data block/data.tf
@@ -1,11 +1,14 @@
-data "aws_ami" "this_ami" {
+data "aws_ami" "this_aws_ami" {
     name_regex       = "ami_use"
       filter {
     name   = "name"
     values = ["ami_use"]
- }
+    }
+
 }
+
 
 data "aws_security_group" "this_sg" {
     name = "default"
 }
+
