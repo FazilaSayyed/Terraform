9fba9571844ac3612c880541b6916727e768cff1
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-14 22:56:19 +0530

    commit

--- Data block/main.tf
+++ Data block/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "this_ubuntu" {
     
-   ami = data.aws_ami.this_aws.id     
+   ami =  data.aws_ami.this_aws.id     
 
    #data.provider_resourcename.uniquename.attribute
 
     disable_api_stop  = var.this_disable_api_stop 
     disable_api_termination = var.this_disable_api_termination  
-    instance_type = var.this_list[0]
+    instance_type = var.this_instance_type
    
     vpc_security_group_ids = data.aws_security_group.this_sg.id
     #count = var.this_count  #loop #var.this_list[1]
