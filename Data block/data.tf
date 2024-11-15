434831574799009bc3c2d7ead4468ef591f53485
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-15 11:32:03 +0530

    Update data.tf

--- Data block/data.tf
+++ Data block/data.tf
@@ -1,12 +1,23 @@
-data "aws_ami" "this_aws_ami" {
-    name_regex       = "ami_use"
-      filter {
+data "aws_ami" "example" {
+    name_regex       = "ami-web"
+    filter {
     name   = "name"
-    values = ["ami_use"]
+    values = ["ami-web"]
+  }  
+}
+
+data "aws_security_group" "default"{
+    name = "default"
+}
+
+/* data "aws_security_groups" "my_sg" {
+   /*  filter {
+    name   = "group-name"
+    values = ["default"]
+    } 
+ 
+filter {
+    name   = "name"
+    values = ["default"]
     }
-
-}
-
-data "aws_security_group" "lb_sg" {
-  name = "default"  #var.vpc_security_group_ids[2]
-}
+} */
