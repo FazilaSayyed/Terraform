2faac27563f97ff69cfa7e9f03e222accf651d42
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-23 12:21:53 +0530

    Update variable.tf

--- provisioners-block/variable.tf
+++ provisioners-block/variable.tf
@@ -25,22 +25,22 @@ variable "this_vpc_cidr_block" {
 
 
 variable "this_vpc_tags" {
-    type = "string"
+    type = string
 }
 
 
 
 variable "this_subnet_pub_cidr_block" {
-    type = "string"
+    type = string
 }
 
 
 
 variable "this_subnet_pub_map_ip" {
-    type = "string"
+    type = string
 }
 
 
 variable "this_subnet_pub_tags" {
-    type = "string"
+    type = string
 }
