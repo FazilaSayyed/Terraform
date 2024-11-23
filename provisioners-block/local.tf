62b71888a6b1706c2f77cfcea36aea155c444671
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-23 14:00:02 +0530

    local

--- provisioners-block/local.tf
+++ provisioners-block/local.tf
@@ -11,15 +11,6 @@ region = "us-west-1"
     key_name = "navidali"
     instance_type = "t2.micro"
      
-     provisioner "file" {
-    source      = "readme.md"
-    destination = "/home/ec2-user/readme.md"
-      connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("${path.module}/id_rsa")
-    host     = "${self.public_ip}"
-  }
   
   
   }
