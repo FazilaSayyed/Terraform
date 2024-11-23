87763fe57e9c2365e453f7aeddabba599e99b340
FazilaSayyed <fazila.sayyed29@gmail.com>
2024-11-23 13:26:08 +0530

    comm

--- dependency-dependon/variable.tf
+++ dependency-dependon/variable.tf
@@ -1,11 +1,55 @@
+variable "this_image_id" {
+   default = ""
+   type = string
+}
+
+variable "this_disable_api_stop" {
+   default = false
+   type = bool
+}
+
+variable "this_disable_api_termination" {
+   default = false
+   type = bool
+}
+
+variable "this_instance_type" {
+   type = string
+}
+
+variable "this_disable_api_termination" {
+   default = false
+   type = bool
+}
+
+
+variable "this_tags" {
+   default = "purpose"
+   type = string
+}
+
+
 variable "this_vpc_cidr_block" {
-  type    = string
-  default = "12.11.0.0/16" # Replace with your desired CIDR block
+   default = "12.11.0.0/16"
+   type = string
 }
 
 variable "this_vpc_tags" {
-  type    = map(string)
-  default = { 
-    Name = "this_vpc" # Replace with your desired tags
-  }
+   default = "this_vpc"
+   type = string
+}
+
+variable "this_subnet_pub_cidr_block" {
+   default = "12.11.0.0/17"
+   type = string
+}
+
+variable "this_subnet_pub_map_ip" {
+   default = true
+   type = bool
+}
+
+variable "this_subnet_pub_tags" {
+    default = "pub_subnet"
+    type = string
 }
